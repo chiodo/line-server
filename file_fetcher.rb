d2e625716ef4a263ea91143fe7d509b609072cce
Chris Chiodo <cchiodo@gmail.com>
2017-02-18 11:37:04 -0500

    Add some comments to clarify what's going on.

--- file_fetcher.rb
+++ file_fetcher.rb
@@ -1,13 +1,11 @@
 require 'thread'
-
-
 class FileFetcher
   def initialize
     #prefetch and load into memory the entire file.
     @semaphore = Mutex.new
     @file_array = []
     @file = File.open( ENV['FILENAME'])
-
+    #run through every line in the file, mark the byte offset of each line in an array.
     loop do
       break if not line = @file.gets
       @file_array[@file.lineno] = @file.pos
@@ -17,7 +15,11 @@ class FileFetcher
   def get_line(number)
     raise RequestedIndexError.new if number >= @file_array.length
     seek_pos = @file_array[number]
+    #synchronized access is a bit faster than reopening the file every time.  Need this mutex because Puma is using
+    #multiple threads for access
     @semaphore.synchronize do
+      #set current file position to the offset for this line, then read the line.
+      #ruby only keeps a bit of the file in memory here, so this is super efficient, memory wise.
       @file.pos = seek_pos
       @file.readline
     end
